5cf754050e47bc10747f4a1b26c0ce13e301031e
Chris Baum <Christopher.Baum@valpo.edu>
2022-03-16 21:25:54 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,52 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord (array){
+  let employee = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return employee
+}
+
+function createEmployeeRecords (employees){
+  return employees.map(x => createEmployeeRecord(x))
+}
+
+function createTimeInEvent (timeStamp){
+  let [date, hour] = timeStamp.split(` `)
+  let timeInEvent = {type: "TimeIn", hour: parseInt(hour, 10), date: date}
+  this.timeInEvents.push(timeInEvent)
+  return this
+}
+
+function createTimeOutEvent (timeStamp){
+  let [date, hour] = timeStamp.split(` `)
+  let timeOutEvent = {type: "TimeOut", hour: parseInt(hour, 10), date: date}
+  this.timeOutEvents.push(timeOutEvent)
+  return this
+}
+
+function hoursWorkedOnDate(date){
+  let dateTimeIn = this.timeInEvents.find(x => x.date == date)
+  let dateTimeOut = this.timeOutEvents.find(x => x.date == date)
+  return (dateTimeOut.hour - dateTimeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+  return hoursWorkedOnDate.call(this, date)*this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+  return srcArray.find(x => x.firstName == firstName)
+}
+
+function calculatePayroll(srcArray){
+  let allWagesEarned = srcArray.map(x => allWagesFor.call(x))
+  return allWagesEarned.reduce((previousValue, currentValue) => previousValue + currentValue);
+}
